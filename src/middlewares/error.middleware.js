ce95b2cb853c231cd4f0f6cc12b5b251f71ac505
MarwaAshraf1812 <marashraf090@gmail.com>
2026-04-04 01:20:12 +0200

    feat: implement global error handling middleware for consistent API error responses

---
+++ src/middlewares/error.middleware.js
@@ -0,0 +1,35 @@
+export const errorHandler = (err, req, res, next) => {
+  let statusCode = res.statusCode || 500;
+  let message = err.message || "Internal Server Error";
+
+  if(err.name === 'CastError') {
+    statusCode = 404;
+    message = "Resource not found";
+  }
+
+  if(err.code === 11000) {
+    statusCode = 400;
+    message = "Resource already exists";
+  }
+
+  if(err.name === 'ValidationError') {
+    statusCode = 400;
+    message = "Validation error";
+  }
+
+  if(err.name === 'JsonWebTokenError') {
+    statusCode = 401;
+    message = "Invalid token";
+  }
+
+  if(err.name === 'TokenExpiredError') {
+    statusCode = 401;
+    message = "Token expired";
+  }
+
+  res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+};
