f0cd7122c564d5970df8c0175fb357b0062c9ef9
MarwaAshraf1812 <marashraf090@gmail.com>
2026-04-04 01:30:15 +0200

    feat: add morgan logging middleware, urlencoded parsing, and a 404 fallback route

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from 'cors';
 import helmet from 'helmet';
+import morgan from 'morgan';
 import cookieParser from "cookie-parser";
 import connectDB from "./config/dbConfig.js";
 import { errorHandler } from "./middlewares/error.middleware.js";
@@ -16,8 +17,19 @@ app.use(cors({
 }));
 
 app.use(helmet());
+
+// log http requests
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+
+
+app.use((req, res, next) => {
+  res.status(404).json({ success: false, message: "Route not found" });
+});
 
 app.use(errorHandler);
 
