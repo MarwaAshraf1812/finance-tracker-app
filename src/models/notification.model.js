e86153ffd038341def78c4e8fe622b647bf77b33
Mohamed Wael <mohame171819@gmail.com>
2026-04-08 13:09:15 +0200

    add notification model

---
+++ src/models/notification.model.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose");
+
+const notificationSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: [true, "User is required"],
+      index: true,
+    },
+
+    title: {
+      type: String,
+      enum: {
+        values: ["budget_alert", "expense_added", "income_added"],
+        message:
+          "Title must be either budget_alert, expense_added, or income_added",
+      },
+      required: [true, "Title is required"],
+    },
+
+    message: {
+      type: String,
+      required: [true, "Message is required"],
+      trim: true,
+    },
+
+    isRead: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+module.exports = mongoose.model("Notification", notificationSchema);
